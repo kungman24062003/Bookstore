c78accdfec4b9be16ad70598d6016586a706c9a6
kungman24062003 <lenhkungman4344@gmail.com>
2025-03-18 17:02:13 +0700

    startup

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Item
-from rest_framework import serializers
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = '__all__'
-
-class ItemListView(APIView):
-    def get(self, request):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data)
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'app/home.html')
 # Create your views here.
